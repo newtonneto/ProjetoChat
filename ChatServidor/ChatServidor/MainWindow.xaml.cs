460f32e45de69ec0028e89ec5a52ad2d3650100e
20182014040016 <newton.n@academico.ifrn.edu.br>
2020-03-15 11:23:25 -0300

    Primeiro Commit

---
+++ ChatServidor/ChatServidor/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+
+namespace ChatServidor
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private delegate void AtualizaStatusCallback(string strMensagem);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAtender_Click(object sender, RoutedEventArgs e)
+        {
+            //Analisa o endereço IP do servidor informado no txtIP
+            IPAddress enderecoIP = IPAddress.Parse(txtIP.Text);
+
+            //Cria uma nova instância do objeto ChatServidor
+            ChatServidor mainServidor = new ChatServidor(enderecoIP);
+
+            //Vincula o tratamento de evento StatusChanged a mainServer_StatusChanged
+            ChatServidor.StatusChanged += new StatusChangedEventHandler(mainServer_StatusChanged);
+
+            //Inicia o atendimento das conexões
+            mainServidor.IniciaAtendimento();
+
+            //Mostra que nos iniciamos o atendimento para conexões
+            txtLog.AppendText("Monitorando as conexões...\r\n");
+        }
+        
+        public void mainServidor_StatusChanged(object sender, StatusChangedEventArgs e)
+        {
+            //Chama o método que atualiza o formulário
+            this.Dispatcher.Invoke(new AtualizaStatusCallback(this.AtualizaStatus), new object[] { e.EventMessage });
+        }
+
+        private void AtualizaStatus(string strMensagem)
+        {
+            //Atualiza o log com mensagens
+            txtLog.AppendText(strMensagem + "\r\n");
+        }
+    }
+}
---
+++ ChatServidor/ChatServidor/Servidor.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Collections;
+
+namespace ChatServidor
+{
+    //Trata os argumento para o evento StatusChanged
+    public class StatusChangedEventArgs : EventArgs
+    {
+        //Mensagem que descreve o evento
+        private string eventMsg;
+
+        //Propriedade para retornar e definir uma mensagem do evento
+        public string EventMessage
+        {
+            get { return eventMsg; }
+            set { eventMsg = value; }
+        }
+
+        //Construtor para definir a mensagem do evento
+        public StatusChangedEventArgs(string strEventMsg)
+        {
+            eventMsg = strEventMsg;
+        }
+    }
+
+    //Delegate necessário para especificar os parametros que estamos passando para o evento
+    public delegate void StatusChangedEventHandler(object sender, StatusChangedEventArgs e);
+
+    class Servidor
+    {
+        //Esta hashtable armazena os usuários e conexões (acessado/consultado por usuário)
+        public static Hashtable htUsuarios = new Hashtable(30); //30 usuários é o limite de conexões
+        //Esta hashtable armazena as conexões e os usuários (acessada/consultada por conexão)
+        public static Hashtable htConexoes = new Hashtable(30); //30 conexões é o limite
+        //Armazena o endereço IP
+        private IPAddress enderecoIP;
+        private TcpClient tcpClient;
+        //O evento e o seu argumento irá notificar o formulário quando um usuário se conecta, desconecta, envia uma mensagem e etc
+        public static event StatusChangedEventHandler StatusChanged;
+        private static StatusChangedEventArgs e;
+
+        //O construtor define o endereço IP para aquele retornado pela instanciação do objeto
+        public Servidor(IPAddress endereco)
+        {
+            enderecoIP = endereco;
+        }
+
+        //A thread que ira tratar o escutador de conexões
+        private Thread thrListener;
+
+        //O objeto TCP object que escuta as conexões
+        private TcpListener tlsClientes;
+
+        //Ira dizer ao laço while para manter a monitoração das conexões
+        bool servRodando = false;
+
+        //Inclui o usuário nas hashtables
+        public static void IncluiUsuario(TcpClient tcpUsuario, string strUsername)
+        {
+            //Primeiro inclui o nome e a conexão associada para ambas as hashtables
+            Servidor.htUsuarios.Add(strUsername, tcpUsuario);
+            Servidor.htConexoes.Add(tcpUsuario, strUsername);
+
+            //Informa a nova conexão para todos os usuários e para o formulário do servidor
+            EnviaMensagemAdmin(htConexoes[tcpUsuario] + " entrou...");
+        }
+
+        //Remove usuários das hashtables
+        public static void RemoveUsuario(TcpClient tcpUsuario)
+        {
+            //Se o usuário existir
+            if (htConexoes[tcpUsuario] != null)
+            {
+                //Primeiro mostra a informaçõa e informa os outros usuário sobre a conexão
+                EnviaMensagemAdmin(htConexoes[tcpUsuario] + " saiu...");
+            }
+        }
+    }
+}
